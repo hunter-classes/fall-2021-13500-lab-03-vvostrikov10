3be221e8d0fb765630510053e3275869acd8531d
Vladislav Vostrikov <vvostrikov@pop-os.localdomain>
2022-09-14 16:19:43 -0400

    Added part D

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <string>
 #include "reservoir.h"
-
+#include "reverseorder.h"
 
 int main(){
     std::cout << "Testing get_east_storage" << std::endl;
@@ -18,7 +18,14 @@ int main(){
     std::cout << compare_basins("01/01/2018") << std::endl;//West
     std::cout << compare_basins("12/30/2018") << std::endl; //West
     std::cout << compare_basins("07/26/2018") << std::endl; //East
-    std::cout << compare_basins("test_equal") << std::endl; //added entry test_equal at the end of the data sheet to test equal elevations
-    
+    std::cout << compare_basins("test_equal") << std::endl; //added entry test_equal at the end of the tsv file to test equal elevations
+    std::cout << "Testing reverse_order" << std::endl;
+    reverse_order("01/01/2018", "01/01/2018");
+    std::cout << std::endl;
+    reverse_order("01/01/2018", "01/15/2018");
+    std::cout << std::endl;
+    reverse_order("05/29/2018", "06/02/2018");
+       
+
     return 0;
 }
